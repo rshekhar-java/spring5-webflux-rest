d4edac4fc495bc93eeeb861728ac5d57e8b69d59
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-27 21:22:46 -0600

    updated Sample Test for initialization Issue

--- src/test/java/com/rs/springframework/Spring5WebfluxRestApplicationTests.java
+++ src/test/java/com/rs/springframework/Spring5WebfluxRestApplicationTests.java
@@ -1,16 +1,16 @@
 package com.rs.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class Spring5WebfluxRestApplicationTests {
+public class Spring5WebfluxRestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoad() {
     }
 
 }
