8de19c5f1b4a8721fa4efd866667cb69cb3745dc
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-28 12:17:18 -0600

    Created POST on Category Controller

--- src/main/java/com/rs/springframework/controllers/CategoryController.java
+++ src/main/java/com/rs/springframework/controllers/CategoryController.java
@@ -2,9 +2,9 @@ package com.rs.springframework.controllers;
 
 import com.rs.springframework.domain.Category;
 import com.rs.springframework.repositories.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,5 +26,11 @@ public class CategoryController {
     @GetMapping("/api/v1/categories/{id}")
     Mono<Category>getByID(@PathVariable String id){
         return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    Mono<Void> create(@RequestBody Publisher<Category> categoryStream){
+        return categoryRepository.saveAll(categoryStream).then();
     }
 }
--- src/test/java/com/rs/springframework/controllers/CategoryControllerTest.java
+++ src/test/java/com/rs/springframework/controllers/CategoryControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * created by rs 2/27/2022.
@@ -52,5 +54,20 @@ public class CategoryControllerTest {
                 .exchange()
                 .expectBody(Category.class);
 
+    }
+
+    @Test
+    public void testCreateCateogry() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().description("descrp").build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
