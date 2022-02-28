7812ec5e4d1fb550a207eba79eda11da7dee34f2
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-28 10:14:04 -0600

    Created Vendor Controller

--- src/test/java/com/rs/springframework/Spring5WebfluxRestApplicationTests.java
+++ src/test/java/com/rs/springframework/Spring5WebfluxRestApplicationTests.java
@@ -9,10 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Spring5WebfluxRestApplicationTests {
 
-/*    @Test
-    public void contextLoads() {
-    }*/
     @Test
-    public void tetsRun(){}
+    public void contextLoads() {
+    }
+
 
 }
